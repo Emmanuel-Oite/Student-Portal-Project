86c1c60ea1f1482b00cdbd4cee5b92f311fa2d15
Emmanuel <opotiemmanuel@gmail.com>
2023-10-23 19:44:14 +0300

    Second commit, proper labelling

--- Index.js
+++ Index.js
@@ -1,96 +1,117 @@
+// Sample JSON data for students
+let students = [];
+
 // Function to populate the student list with courses and grades
 function populateStudentList() {
-  const studentList = document.getElementById("student-list");
-  studentList.innerHTML = ''; // Clear the existing list
+    const studentList = document.getElementById("student-list");
+    studentList.innerHTML = ''; // Clear the existing list
 
-  // Load the student data from the external JSON file
-  fetch('students.json')
-      .then(response => response.json())
-      .then(data => {
-          students = data;
-          students.forEach((student, index) => {
-              const studentCard = document.createElement("div");
-              studentCard.classList.add("student");
-              studentCard.innerHTML = `
-                  <h2>Name: ${student.name}</h2>
-                  <p>Age: ${student.age}</p>
-                  <p>Major: ${student.major}</p>
-                  <h3>Courses:</h3>
-                  <ul>
-                      ${student.courses.map((course, courseIndex) => `
-                          <li>${course.courseName} (Grade: ${course.grade}) 
-                          <button onclick="updateGrade(${index}, ${courseIndex})">Update Grade</button></li>`).join('')}
-                  </ul>
-                  <button onclick="editStudent(${index})">Edit</button>
-                  <button onclick="deleteStudent(${index})">Delete</button>
-              `;
-              studentList.appendChild(studentCard);
-          });
-      })
-      .catch(error => console.error('Error loading student data:', error));
+    // Load the student data from the external JSON file
+    fetch('students.json')
+        .then(response => response.json())
+        .then(data => {
+            students = data;
+            students.forEach((student, index) => {
+                const studentCard = document.createElement("div");
+                studentCard.classList.add("student");
+                studentCard.innerHTML = `
+                    <h2>Name: ${student.name}</h2>
+                    <p>Age: ${student.age}</p>
+                    <p>Major: ${student.major}</p>
+                    <h3>Courses:</h3>
+                    <ul>
+                        ${student.courses.map((course, courseIndex) => `
+                            <li>${course.courseName} (Grade: ${course.grade}) 
+                            <button onclick="updateGrade(${index}, ${courseIndex})">Update Grade</button></li>`).join('')}
+                    </ul>
+                    <button onclick="editStudent(${index})">Edit</button>
+                    <button onclick="deleteStudent(${index})">Delete</button>
+                `;
+                studentList.appendChild(studentCard);
+            });
+        })
+        .catch(error => console.error('Error loading student data:', error));
 }
 
 // Function to update a student's grade
 function updateGrade(studentIndex, courseIndex) {
-  const newGrade = prompt("Enter the new grade:");
+    const newGrade = prompt("Enter the new grade:");
 
-  if (newGrade) {
-      students[studentIndex].courses[courseIndex].grade = newGrade;
-      saveStudentData();
-  }
+    if (newGrade) {
+        students[studentIndex].courses[courseIndex].grade = newGrade;
+        saveStudentData();
+    }
 }
 
 // Function to add a new student
 function addStudent() {
-  const newName = prompt("Enter the name of the new student:");
-  const newAge = prompt("Enter the age of the new student:");
-  const newMajor = prompt("Enter the major of the new student:");
+    const newName = prompt("Enter the name of the new student:");
+    const newAge = prompt("Enter the age of the new student:");
+    const newMajor = prompt("Enter the major of the new student:");
 
-  if (newName && newAge && newMajor) {
-      students.push({
-          name: newName,
-          age: newAge,
-          major: newMajor,
-          courses: []  // Initialize with an empty course array
-      });
-      saveStudentData();
-  }
+    if (newName && newAge && newMajor) {
+        students.push({
+            name: newName,
+            age: newAge,
+            major: newMajor,
+            courses: []  // Initialize with an empty course array
+        });
+        saveStudentData();
+    }
 }
 
 // Function to edit a student's information
 function editStudent(index) {
-  const updatedName = prompt("Enter the updated name:");
-  const updatedAge = prompt("Enter the updated age:");
-  const updatedMajor = prompt("Enter the updated major:");
+    const modal = document.getElementById('edit-student-modal');
+    modal.style.display = 'block';
 
-  if (updatedName && updatedAge && updatedMajor) {
-      students[index].name = updatedName;
-      students[index].age = updatedAge;
-      students[index].major = updatedMajor;
-      saveStudentData();
-  }
+    // Fill the edit form with the current student data
+    const nameInput = document.getElementById('edit-student-name');
+    nameInput.value = students[index].name;
+
+    const ageInput = document.getElementById('edit-student-age');
+    ageInput.value = students[index].age;
+
+    const majorInput = document.getElementById('edit-student-major');
+    majorInput.value = students[index].major;
+
+    // Save the edited data on "Save" button click
+    const saveButton = document.getElementById('save-edit-btn');
+    saveButton.onclick = () => {
+        students[index].name = nameInput.value;
+        students[index].age = ageInput.value;
+        students[index].major = majorInput.value;
+        saveStudentData();
+        modal.style.display = 'none';
+    };
+
+    // Close the modal on "x" click
+    const closeButton = document.getElementsByClassName('close')[0];
+    closeButton.onclick = () => {
+        modal.style.display = 'none';
+    };
 }
 
 // Function to delete a student from the database
 function deleteStudent(index) {
-  students.splice(index, 1);
-  saveStudentData();
+    students.splice(index, 1);
+    saveStudentData();
 }
 
 // Function to save student data back to the JSON file
 function saveStudentData() {
-  // Save the updated student data to the external JSON file
-  fetch('students.json', {
-      method: 'POST',
-      body: JSON.stringify(students),
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  })
-  .then(() => {
-      populateStudentList(); // Refresh the student list with updated data
-  })
-  .catch(error => console.error('Error saving student data:', error));
+    // Save the updated student data to the external JSON file
+    fetch('students.json', {
+        method: 'POST',
+        body: JSON.stringify(students),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(() => {
+        populateStudentList(); // Refresh the student list with updated data
+    })
+    .catch(error => console.error('Error saving student data:', error));
 }
 
 // Call the function to populate the student list
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Student-Portal-Project
-Simple project for a student portal for a small institute
+
+# Student Directory Web Application
+
+The Student Directory Web Application is a simple web-based tool for managing student information, course lists, and grades. It allows you to view and edit student data, including student names, ages, majors, courses, and grades.
+
+## Features
+
+- View a list of students with their details, courses, and grades.
+- Edit a student's information, including their name, age, and major.
+- Add new students to the directory with their details.
+- Delete students from the directory.
+- Update grades for individual courses.
+
+## How to Use
+
+1. Open the `index.html` file in your web browser.
+
+2. You will see a list of students with their information, including courses and grades.
+
+3. To edit a student's information, click the "Edit" button on the student card. You can change the name, age, and major.
+
+4. To add a new student, click the "Add Student" button at the top of the page. You will be prompted to enter the new student's name, age, and major. The student will be added to the directory with an empty course list.
+
+5. To delete a student, click the "Delete" button on the student card. The student and their information will be removed from the directory.
+
+6. To update a student's grade for a specific course, click the "Update Grade" button next to the course name. You will be prompted to enter the new grade for that course.
+
+## Data Storage
+
+The student data is stored in a separate JSON file named `students.json`. When you make changes to the student directory, the application will save the updated data back to this JSON file.
+
+## Contributions
+
+Feel free to contribute to this project by improving the code, adding new features, or fixing issues. Please fork the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. You can find more information in the `LICENSE` file.
+
+---
+
+Enjoy using the Student Directory Web Application for managing student information and grades!
